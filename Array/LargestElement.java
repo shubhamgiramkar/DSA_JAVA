b559cc299b53b4557bc38291cfc4b37ae4fc56aa
Shubham Giramkar <shubhamgiramkar1212@gmail.com>
2023-08-13 19:48:37 +0530

    third commit

--- Array/LargestElement.java
+++ Array/LargestElement.java
@@ -6,12 +6,13 @@ public class LargestElement{
        Scanner sc=new Scanner(System.in);
        System.out.println("Enter Size Of Array");
        int size=sc.nextInt();
- int element=0;
+ 
         int arr[]=new int[size];
         System.out.println("Enter Elements In Array");
         for(int i=0;i<size;i++){
             arr[i]=sc.nextInt();
         }
+        int element=arr[0];
         for(int i=0;i<size;i++){
             if(element<arr[i]){
              element=arr[i];
