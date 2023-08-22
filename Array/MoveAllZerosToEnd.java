c72baaf3d7d507b0afc02b48fc712d1703e22ddb
Shubham Giramkar <shubhamgiramkar1212@gmail.com>
2023-08-22 12:24:34 +0530

    final Commit

--- Array/MoveAllZerosToEnd.java
+++ Array/MoveAllZerosToEnd.java
@@ -17,17 +17,36 @@ System.out.println("Before Operation");
 MoveZeros(arr,size);
 }
 public static void MoveZeros(int[] arr,int size){
-    int temp=0,count=0;
-    for(int i=0;i<size-1;i++){
-        while(arr[i]==0){
-         count++;
-         for(int j=i;j<size-1;j++){
-         arr[j]=arr[j+1];}
+    int count=0;
+    int temp_count=0;
+    /* we count the the total zeros in array */
+    for(int i=0;i<size;i++){
         
+        if(arr[i]==0){
+           count++;
         }
-        arr[size-1]=temp;
-        count=0;
+    }
+    /*we replace the element inside yhe zero elemnt */
+    
+    for(int i=0;i<size;i++){
         
+        
+        if(arr[i]==0){
+           temp_count++;
+           //continue;
+        }else{
+            arr[i-temp_count]=arr[i];
+            temp_count=0;
+        }
+    }
+    //this statement adds zeros at the end of array after total count of non-zero emlements
+    // suppose we have 2 non zero elements in array afetr this operation we add zoros after index number
+    //of 1 so lastindex of non zero element = total size-number of zero elemnet
+   int newsize=size-count;
+
+    for(int i=newsize;i<size;i++){
+       
+        arr[i]=0;
     }
     System.out.println("After Operation");
     for(int i=0;i<size;i++){
